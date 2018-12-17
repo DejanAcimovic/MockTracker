0916e8e70a4032684b36f018d30a097cac2eb399
Nermin <nermin.milisic@edu.fit.ba>
2018-12-17 15:51:50 +0100

    [lejla nermin] minor changes to timesheetservice http

--- MockTrackerAngular/src/app/timesheet.service.ts
+++ MockTrackerAngular/src/app/timesheet.service.ts
@@ -3,23 +3,27 @@ import { Observable, of } from 'rxjs';
 import { TimeSheet } from './timesheet';
 import { TIMESHEETS, TASKTYPES } from './mock-timesheet';
 import { TaskType } from './tasktype';
-import {HttpClient,HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
 @Injectable({
   providedIn: 'root'
 })
 export class TimesheetService {
 
-private timeSheetUrl='co'
-  constructor(private http:HttpClient) { 
-     
+  private timeSheetTasksUrl = 'https://localhost:44394/api/timesheet/tasks';
+  constructor(private http: HttpClient) {
+
   }
 
   getTimeSheets(): Observable<TimeSheet[]> {
-     return of(TIMESHEETS)
+    return of(TIMESHEETS)
   }
 
   getTaskType(): Observable<TaskType[]> {
-    return this.http.get<TaskType[]>(this.timeSheetUrl)
- }
+    return this.http.get<TaskType[]>(this.timeSheetTasksUrl)
+  }
 }
--- MockTrackerAngular/src/app/timesheet/timesheet.component.ts
+++ MockTrackerAngular/src/app/timesheet/timesheet.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { TimeSheet } from '../timesheet';
 import { TimesheetService } from '../timesheet.service';
-import {MatPaginator, MatTableDataSource} from '@angular/material';
 import { TaskType } from '../tasktype';
 @Component({
   selector: 'app-timesheet',
@@ -10,58 +9,37 @@ import { TaskType } from '../tasktype';
 })
 export class TimesheetComponent implements OnInit {
 
-  sheets:TimeSheet[]=[];
- // displayedColumns: string[] = ['Task type', 'Duration', 'Billable', 'symbol'];
+  sheets: TimeSheet[] = [];
+  // displayedColumns: string[] = ['Task type', 'Duration', 'Billable', 'symbol'];
   //dataSource = new MatTableDataSource<TimeSheet>(this.sheets);
-tasktypes:TaskType[]=[];
-  displayedColumns: string[] = ['No.','Task', 'Duration', 'Billable'];
-  dataSource ;
-
- 
+  tasktypes: TaskType[] = [];
+  displayedColumns: string[] = ['No.', 'Task', 'Duration', 'Billable'];
+  dataSource;
 
 
-  constructor(private timesheetService:TimesheetService) 
-  {
 
 
-   }
+  constructor(private timesheetService: TimesheetService) { }
 
   ngOnInit() {
-   
-     this.GetSheets();
-     this.dataSource=this.sheets;
-     
-  }
 
-  GetSheets():void{
-  this.timesheetService.getTimeSheets().subscribe(sheets=>this.sheets=sheets)
+    this.GetSheets();
+    this.dataSource = this.sheets;
+    this.GetTaskTypes();
+
 
   }
 
-  GetTaskTypes():void{
-    this.timesheetService.getTimeSheets().subscribe(sheets=>this.sheets=sheets)
-  
-    }
+  GetSheets(): void {
+    this.timesheetService.getTimeSheets().subscribe(sheets => this.sheets = sheets)
 
-  
+  }
+
+  GetTaskTypes(): void {
+    this.timesheetService.getTaskType().subscribe(tasks => this.tasktypes = tasks)
+    //console.log(this.tasktypes);
+
+  }
+
+
 }
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
