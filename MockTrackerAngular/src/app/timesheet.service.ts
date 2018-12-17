612d84cf372d5624b112de80316c13b9297549b2
Lejla Špago <lejla.spago@edu.fit.ba>
2018-12-17 15:05:21 +0100

    commit

--- MockTrackerAngular/src/app/timesheet.service.ts
+++ MockTrackerAngular/src/app/timesheet.service.ts
@@ -3,20 +3,23 @@ import { Observable, of } from 'rxjs';
 import { TimeSheet } from './timesheet';
 import { TIMESHEETS, TASKTYPES } from './mock-timesheet';
 import { TaskType } from './tasktype';
+import {HttpClient,HttpHeaders} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TimesheetService {
 
-
-  constructor() { }
+private timeSheetUrl='co'
+  constructor(private http:HttpClient) { 
+     
+  }
 
   getTimeSheets(): Observable<TimeSheet[]> {
      return of(TIMESHEETS)
   }
 
   getTaskType(): Observable<TaskType[]> {
-    return of(TASKTYPES)
+    return this.http.get<TaskType[]>(this.timeSheetUrl)
  }
 }
